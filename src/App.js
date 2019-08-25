c1fe54b01956303372555e3b17a45d3cf7fa507b
Jason B <philotimo@gmail.com>
2019-08-25 19:18:19 -0400

    update pkg.json with correct homepage correct deploy script name and gh-pages dependency

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
 import RollDice from './RollDice';
 import './App.css';
 
 class App extends Component {
   render(){
     return (
+      <HashRouter basename='/'>
       <div className="App">
         <RollDice />
+        <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
       </div>
+      </HashRouter>
     );
   } 
 }
+
+const Home = () => <div> <h2> Home </h2></div>
+const About = () => <div> <h2> About </h2></div>
 
 export default App;
