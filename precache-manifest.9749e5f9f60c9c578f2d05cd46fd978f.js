88a0db847bfbb736fb256bfaf5bb7c020092d503
Jason B <philotimo@gmail.com>
2019-08-25 19:10:27 -0400

    Updates

---
+++ precache-manifest.9749e5f9f60c9c578f2d05cd46fd978f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a1c5d45524001c1b27f4676d7e0329",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "2d05c473047b0c58f006",
+    "url": "/dice/static/css/main.9bffd07b.chunk.css"
+  },
+  {
+    "revision": "ba40044bfa465cc4cd9f",
+    "url": "/dice/static/js/2.a0e75d90.chunk.js"
+  },
+  {
+    "revision": "2d05c473047b0c58f006",
+    "url": "/dice/static/js/main.611cb14d.chunk.js"
+  },
+  {
+    "revision": "22cb0124cc2e7d27f73f",
+    "url": "/dice/static/js/runtime~main.63d5d763.js"
+  }
+]);
